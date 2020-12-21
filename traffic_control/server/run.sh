cd54eb7aeab55443ce656c0a82ca0bf9530ad713
Ivan Ignatyev <ignatev.is@phystech.edu>
2020-12-22 01:32:14 +0300

    Minor fix default class

--- traffic_control/server/run.sh
+++ traffic_control/server/run.sh
@@ -11,7 +11,7 @@ tc class add dev ifb0 parent 1:0 classid 1:1 htb rate 100mbit prio 1
 tc filter add dev ifb0 protocol ip parent 1:0 prio 1 u32 match ip protocol 1 0xff action drop
 tc filter add dev ifb0 protocol ip parent 1:0 prio 1 u32 match ip src 172.20.3.0/24 match ip protocol 6 0xff match ip dport 22 0xffff action drop
 
-tc qdisc add dev eth0 root handle 1:0 htb default 11
+tc qdisc add dev eth0 root handle 1:0 htb default 14
 tc class add dev eth0 parent 1:0 classid 1:10 htb rate 100mbit prio 1
 tc class add dev eth0 parent 1:10 classid 1:11 htb rate 5mbit ceil 40mbit prio 1
 tc class add dev eth0 parent 1:10 classid 1:12 htb rate 5mbit ceil 20mbit prio 1
