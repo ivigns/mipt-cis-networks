2fabc29b4e50467ea117723b624db8cf77a167e8
Ivan Ignatyev <ignatev.is@phystech.edu>
2020-12-22 01:22:39 +0300

    Add traffic control

--- traffic_control/client/run.sh
+++ traffic_control/client/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "=== SEND TCP ==="
-iperf -c 172.20.1.0 -p 8080 -t 5
-echo "=== SEND SSH ==="
-sshpass -p "test" ssh -oStrictHostKeyChecking=no -i id_rsa test@172.20.1.0 \
-    echo "successful ssh request"
-echo "=== SEND ICMP ==="
-ping 172.20.1.0 -c 5
-echo "=== SEND UDP ==="
-iperf -c 172.20.1.0 -u -p 8080 -t 5
-echo "=== DONE ==="
+iperf -seu -p 8080 &
+iperf -se -p 8080 &
+
+while true; do
+    sshpass -p "test" ssh -oStrictHostKeyChecking=no -oConnectTimeout=5 \
+    -i id_rsa test@172.20.1.0 echo "successful ssh request"
+    ping 172.20.1.0 -c 5
+done
--- traffic_control/server/run.sh
+++ traffic_control/server/run.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-# todo: tc
+ip link add ifb0 type ifb
+ip link set ifb0 up
+ip a
+
+tc qdisc add dev eth0 ingress
+tc filter add dev eth0 parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
+tc qdisc add dev ifb0 root handle 1:0 htb default 1
+tc class add dev ifb0 parent 1:0 classid 1:1 htb rate 100mbit prio 1
+tc filter add dev ifb0 protocol ip parent 1:0 prio 1 u32 match ip protocol 1 0xff action drop
+tc filter add dev ifb0 protocol ip parent 1:0 prio 1 u32 match ip src 172.20.3.0/24 match ip protocol 6 0xff match ip dport 22 0xffff action drop
+
+tc qdisc add dev eth0 root handle 1:0 htb default 11
+tc class add dev eth0 parent 1:0 classid 1:10 htb rate 100mbit prio 1
+tc class add dev eth0 parent 1:10 classid 1:11 htb rate 5mbit ceil 40mbit prio 1
+tc class add dev eth0 parent 1:10 classid 1:12 htb rate 5mbit ceil 20mbit prio 1
+tc class add dev eth0 parent 1:10 classid 1:121 htb rate 5mbit ceil 20mbit prio 2
+tc class add dev eth0 parent 1:10 classid 1:13 htb rate 5mbit ceil 20mbit prio 1
+tc class add dev eth0 parent 1:10 classid 1:14 htb rate 5mbit ceil 20mbit prio 1
+tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst 172.20.1.0/24 flowid 1:11
+tc filter add dev eth0 protocol ip parent 1:0 prio 2 u32 match ip dst 172.20.2.0/24 match ip protocol 17 0xff flowid 1:121
+tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst 172.20.2.0/24 flowid 1:12
+tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst 172.20.3.0/24 flowid 1:13
+tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst 172.20.4.0/24 flowid 1:14
 
 service ssh start
 service ssh status
-iperf -su -p 8080 &
-iperf -s -p 8080
+
+while true; do
+    sleep 5
+    iperf -c 172.20.1.1 -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.1.1 -u -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.2.2 -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.2.2 -u -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.3.3 -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.3.3 -u -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.4.4 -p 8080 -e -i 1 -t 1 &
+    iperf -c 172.20.4.4 -u -p 8080 -e -i 1 -t 1 &
+done
